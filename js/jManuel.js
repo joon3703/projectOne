000ff59f5c7fe310304efb97ad932dc1c7a81249
TMSulik <shulich@hotmail.com>
2019-02-11 21:27:11 -0800

    Cleaned up version to submit as our class project

--- js/jManuel.js
+++ js/jManuel.js
@@ -12,30 +12,60 @@ var config = {
 // Establish database
 var database = firebase.database();
 
-    // Authentication function
-    function submitCreateAccount() {
-    // Authentication variables
-    var displayName = document.querySelector("#entry-displayname");
-    var email = document.querySelector("#entry-email");
-    var password = document.querySelector("#entry-password");
-    
-    firebase.auth().createUserWithEmailAndPassword(email.value, paswordpassword.value)
-        .then(function(user) {
-            // add the displayName
-            user.updateProfile({displayName: displayName.value});
-    
-    });
-    
+// ================================================= //
+//  GLOBAL VARIABLES: CHATBOX
+// ================================================= //
+
+// Reference chat
+var chatRef = database.ref('/chat');
+
+// DOM cache chat
+var $chatBtn = $('#chatBtn');
+var $chatInput = $('#text-area');
+var $chatUi = $('#chat-window').find('ul');
+var userName = "";
+
+// ================================================= //
+//  DATABASE LISTENER: CHATBOX
+// ================================================= //
+
+// Create a realtime listener for incoming messages
+chatRef.on('child_added', function(childSnapshot) {
+
+    // Add message value into a variable
+    var message = childSnapshot.val();
+    console.log("msg: " + message);
+
+    // Get name of sender
+    // userName = $logUser.val().trim();
+
+    // Append the new message to the chat window
+    $chatUi.append("<li class='msg-entry'>" + message + "</li>");
+
+});
+
+// ================================================= //
+//  BUTTON EVENT: SEND MESSAGE 
+// ================================================= //
+
+// Create a submit message event (click and keypress:enter)
+function submit() {  
+
+    // Push the current message from the text input onto the database
+    chatRef.push($chatInput.val().trim(),
+    );
+
+    // Clear text input
+    $chatInput.val('');  
 }
 
-// Chat function
-function sendChatMessage() {
-    ref = firebase.databse().ref("/chat");
-    messageField = document.querySelector("#chat-message");
+// Trigger submit function: click & enter
+$chatBtn.click(function() {
+    submit();
+});
 
-    ref.push().set({
-        name: firebase.auth().currentUser.displayName,
-        message: messageField.value
-    }); 
-    
-}
+$(document).on('keypress',function(event) {
+    if(event.which == 13) {
+        submit();
+    }
+});
--- js/script.js
+++ js/script.js
@@ -1,14 +1,188 @@
+var whitePlayer = "White";
+var blackPlayer = "Black";
+
+$("#white-login-button").on("click", function(event) {
+  if($("#white-sign-in").val() === "") {
+    alert("Type your name in the field");
+    return;
+  }
+  whitePlayer = $("#white-sign-in").val().trim();
+
+  $("#white-logout-button").text(whitePlayer + " sign-out");
+  disableSignin('.white-input', '#white-login-button', '#white-logout-button');
+  
+  // Update Firebase database for the two players
+  // when white logs in.
+  updateDatabase();
+});
+
+$("#black-login-button").on("click", function(event) {
+  if($("#black-sign-in").val() === "") {
+    alert("Type your name in the field");
+    return;
+  }
+  blackPlayer = $("#black-sign-in").val().trim();
+
+  $("#black-logout-button").text(blackPlayer + " sign-out");
+  disableSignin('.black-input', '#black-login-button', '#black-logout-button');
+  
+  // Update Firebase database for the two players
+  // when black logs in.
+  updateDatabase();
+});
+
+function disableSignin(player, login, logout) {
+  $(player).hide();
+  $(login).hide();
+  $(logout).show();
+}
+
+$("#white-logout-button").on("click", function(event) {
+  whitePlayer = "White";
+  $("#white-logout-button").hide();
+  $('#white-sign-in').val('');
+  $('#white-sign-in').next().removeClass('active');
+  $(".white-input").show();
+  $("#white-login-button").show();
+  updateDatabase();
+});
+
+$("#black-logout-button").on("click", function(event) {
+  blackPlayer = "Black";
+  $("#black-logout-button").hide();
+  $('#black-sign-in').val('');
+  $('#black-sign-in').next().removeClass('active');
+  $(".black-input").show();
+  $("#black-login-button").show();
+  updateDatabase();
+});
+
+function resetGame() {
+  $("#white-logout-button").hide();
+  $("#black-logout-button").hide();
+  $(".white-input").show();
+  $(".black-input").show();
+  $("#white-login-button").show();
+  $("#black-login-button").show();
+
+}
+
+function updateDatabase() {
+  event.preventDefault();
+  database.ref().set({
+    whitePlayer: whitePlayer,
+    blackPlayer: blackPlayer
+  });
+}
+
+// do not pick up pieces if the game is over
+// only pick up pieces for White
+var onDragStart = function(source, piece, position, orientation) {
+  if (game.in_checkmate() === true || game.in_draw() === true ||
+    piece.search(/^b/) !== -1) {
+    return false;
+  }
+};
+
+var onDrop = function(source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  });
+
+  // illegal move
+  if (move === null) return 'snapback';
+
+  // highlight white's move
+  removeHighlights('white');
+  boardEl.find('.square-' + source).addClass('highlight-white');
+  boardEl.find('.square-' + target).addClass('highlight-white');
+
+  // make random move for black
+  window.setTimeout(makeRandomMove, 250);
+};
+
+var onMoveEnd = function() {
+  boardEl.find('.square-' + squareToHighlight)
+    .addClass('highlight-black');
+};
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+var onSnapEnd = function() {
+  board.position(game.fen());
+};
+
+var cfg = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onMoveEnd: onMoveEnd,
+  onSnapEnd: onSnapEnd
+};
+board = ChessBoard('board', cfg);
+
+// Show position in FEN
+function clickGetPositionBtn() {
+    
+  $('#fen').html(board.fen());
+}
+
+$('#submitBtn').on('click', clickGetPositionBtn);
+
+var onSnapEnd = function() { 
+  console.log(board.position());
+};
+
 var board = ChessBoard('board', {
   draggable: true,
   dropOffBoard: 'trash',
-  sparePieces: true
+  sparePieces: true,
+  onSnapEnd: onSnapEnd
 });
 
 $('#startBtn').on('click', board.start);
 $('#clearBtn').on('click', board.clear);
 
+// Extra stuff
+var pgnEl = $('#pgn');
+
+var updateStatus = function() {
+  var status = '';
+
+  var moveColor = 'White';
+  if (game.turn() === 'b') {
+    moveColor = 'Black';
+  }
+
+  // checkmate?
+  if (game.in_checkmate() === true) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.';
+  }
+
+  // draw?
+  else if (game.in_draw() === true) {
+    status = 'Game over, drawn position';
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move';
+
+    // check?
+    if (game.in_check() === true) {
+      status += ', ' + moveColor + ' is in check';
+    }
+  }
+  pgnEl.html(game.pgn());
+};
+
 $(window).resize(board.resize);
 
+// TODO: Incorporate onChange function to generate the scoresheet 
 // onChange function starts here.
 // Open the console log and move a piece.
 // This function takes in the old position (of the entire board, 
@@ -21,26 +195,27 @@ $(window).resize(board.resize);
 // Forsyth–Edwards Notation:
 // https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation#Examples
 
-var onChange = function(oldPos, newPos) {
-  console.log("Position changed:");
-  console.log("Old position: " + ChessBoard.objToFen(oldPos));
-  console.log("New position: " + ChessBoard.objToFen(newPos));
-  console.log("--------------------");
-};
+// var onChange = function(oldPos, newPos) {
+//   console.log("Position changed:");
+//   console.log("Old position: " + ChessBoard.objToFen(oldPos));
+//   console.log("New position: " + ChessBoard.objToFen(newPos));
+//   console.log("--------------------");
+// };
 
-var cfg = {
-  draggable: true,
-  position: 'start',
-  onChange: onChange
-};
+// var cfg = {
+//   draggable: true,
+//   position: 'start',
+//   onChange: onChange
+// };
 
-var board = ChessBoard('board', cfg);
+// var board = ChessBoard('board', cfg);
 
-$('#startPositionBtn').on('click', board.start);
+// $('#startPositionBtn').on('click', board.start);
 
 // onChange ends here.
-// This function disables the original START and CLEAR buttons.
 
+
+// These are sample moves to show how the scoresheet should look.
 var moves = [
   { white:"e4" , black:"e5" },
   { white:"Nf3" , black:"Nc6" },
@@ -84,4 +259,5 @@ function adjustForSmallScreen() {
 
 $( document ).ready(function() {
   notateMoves();
+  resetGame();
 });
